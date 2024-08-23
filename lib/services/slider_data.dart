2bdc66719ff13fe9b4288e19c3e506d6f1dfd40f
Shivangi singh <84102562+shivs3114@users.noreply.github.com>
2024-08-23 07:31:42 +0530

    Update slider_data.dart

--- lib/services/slider_data.dart
+++ lib/services/slider_data.dart
@@ -8,7 +8,7 @@ class Sliders {
 
   Future<void> getSlider() async {
     String url =
-        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=8eb940539c874fa98a2050d4afde5d5b";
+        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=";
     var response = await http.get(Uri.parse(url));
 
     var jsonData = jsonDecode(response.body);
